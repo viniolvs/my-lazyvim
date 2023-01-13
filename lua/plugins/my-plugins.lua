4a9af4da222f756a39c8b65502d7b0b0fd1a01f3
Vinícius <vinioliveira.s@hotmail.com>
2023-01-12 23:14:06 -0300

    (add) python formatter

--- lua/plugins/my-plugins.lua
+++ lua/plugins/my-plugins.lua
@@ -25,8 +25,22 @@ return {
   {
     "mfussenegger/nvim-jdtls",
     lazy = false,
-    -- config = function ()
-    --   require('lspconfig').jdtls.setup{}
-    -- end
+  },
+
+  -- formatters
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    event = "BufReadPre",
+    dependencies = { "mason.nvim" },
+    opts = function()
+      local nls = require("null-ls")
+      return {
+        sources = {
+          nls.builtins.formatting.black,
+          nls.builtins.formatting.stylua,
+          nls.builtins.diagnostics.flake8,
+        },
+      }
+    end,
   },
 }
