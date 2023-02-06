a413654f579ce5cd2c020c2c856778460c1909c1
Vinícius <vinioliveira.s@hotmail.com>
2023-02-06 19:56:06 -0300

    add: extra modules

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,8 +13,9 @@ require("lazy").setup({
     -- import/override with your plugins
     { import = "plugins" },
     -- import any extras modules here
-    -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.json" },
   },
   defaults = {
     lazy = true, -- every plugin is lazy-loaded by default
