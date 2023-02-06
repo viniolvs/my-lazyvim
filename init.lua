3d9103e5c85b3e652ed91648aa6bb5a637fca4fc
Vinicius de Oliveira Silva <vinioliveira.s@hotmail.com>
2023-02-06 14:09:24 -0300

    fix: organizing files

--- init.lua
+++ init.lua
@@ -10,22 +10,3 @@ require("plugins.disabled")
 
 -- colorscheme
 vim.cmd([[colorscheme dracula]])
-
--- node path
-local home_dir = "/home/vinicius/"
--- pin nvim to a specific node version, regardless of the project
--- prereq - need to install: nvm i 16.17.1
-local node_bin = "/bin/node"
-vim.g.node_host_prog = home_dir .. node_bin .. "/node"
-
--- for mason.nvim
--- prereq - install lsp server in that node/bin npm i -g typescript-language-server
--- (handled by :Mason currently)
-vim.cmd("let $PATH = '" .. home_dir .. node_bin .. ":' . $PATH")
-
-vim.g.copilot_no_tab_map = true
-vim.api.nvim_set_keymap("i", "<C-c>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.offsetEncoding = { "utf-16" }
-require("lspconfig").clangd.setup({ capabilities = capabilities })
--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,3 +2,19 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 vim.api.nvim_create_autocmd("BufEnter", { command = "set nospell" })
+
+-- node path
+local home_dir = "/home/vinicius/"
+-- pin nvim to a specific node version, regardless of the project
+-- prereq - need to install: nvm i 16.17.1
+local node_bin = "/bin/node"
+vim.g.node_host_prog = home_dir .. node_bin .. "/node"
+
+-- for mason.nvim
+-- prereq - install lsp server in that node/bin npm i -g typescript-language-server
+-- (handled by :Mason currently)
+vim.cmd("let $PATH = '" .. home_dir .. node_bin .. ":' . $PATH")
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.offsetEncoding = { "utf-16" }
+require("lspconfig").clangd.setup({ capabilities = capabilities })
