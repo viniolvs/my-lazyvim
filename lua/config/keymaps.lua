9d073ccbba0fa1a70f1c6ebe032b5f8336f9165f
Vinicius de Oliveira Silva <vinioliveira.s@hotmail.com>
2023-03-09 10:06:51 -0300

    add: zen mode keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
---
+
+-- Zen mode
+vim.api.nvim_set_keymap("n", "<leader>z", ":ZenMode<CR>", { silent = true, noremap = true })
 
 -- Harpoon
 vim.api.nvim_set_keymap(
