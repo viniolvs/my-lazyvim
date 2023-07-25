e51a18f3cb11832d1c7b36896ea4122bb50f2102
vinicius <viniciusolvs@proton.me>
2023-07-25 17:12:28 -0300

    add: lazyvim extra java

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,6 +14,7 @@ require("lazy").setup({
     { import = "plugins" },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.lang.java" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
   },
